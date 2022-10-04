e60df534e1004f23948ec6c8ba57b24b2a1e466a
LexiCodes <lexicodes@lexicodes.net>
2022-10-04 15:17:48 +0400

    Update

--- src/main/java/net/lexicodes/kesandra/Kesandra.java
+++ src/main/java/net/lexicodes/kesandra/Kesandra.java
@@ -3,12 +3,14 @@ package net.lexicodes.kesandra;
 import net.lexicodes.kesandra.commands.Commandrepair;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class Kesandra extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        this.getCommand("repair").setExecutor(new Commandrepair());
+        Objects.requireNonNull(this.getCommand("repair")).setExecutor(new Commandrepair());
     }
 
     @Override
--- src/main/java/net/lexicodes/kesandra/commands/Commandrepair.java
+++ src/main/java/net/lexicodes/kesandra/commands/Commandrepair.java
@@ -1,6 +1,5 @@
 package net.lexicodes.kesandra.commands;
 
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,15 +7,22 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public class Commandrepair implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         Player player = (Player) sender;
         ItemStack item = player.getItemInHand();
-        if(item == null || item.getType().isBlock() || item.getDurability() == 0) {
-            player.sendMessage("That Is Not A Repairable Item");
-        } else {
-            item.setDurability((short) 5000);
+
+        if(item.getType().isBlock()) {
+            player.sendMessage("You Cannot Repair A Block!");
+        } else if(Objects.requireNonNull(item.getItemMeta()).isUnbreakable()) {
+            player.sendMessage("This Item Is Unbreakable Or Not Repairable!");
+        } else if(item.getDurability() == 0 ) {
+            player.sendMessage("This Item Is Already At Max Durability!");
+        }else {
+                item.setDurability((short) -3276);
         }
         return false;
     }
